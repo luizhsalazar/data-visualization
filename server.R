8aa7987b7e835b7392ae95f09aa25dc83fe727c6
Rafael Dalsenter <rdalsenter@outlook.com>
2018-12-01 10:46:34 -0200

    pequeno ajuste

--- server.R
+++ server.R
@@ -121,7 +121,7 @@ server <- function(input, output) {
     group_by_(.dots=c("ANO_CONCESSAO_BOLSA", "NOME_CURSO_BOLSA_NOVO")) %>% 
     summarize(total = n()) %>%
     filter(NOME_CURSO_BOLSA_NOVO %in% 
-             c("Administração", "Direito", "Ciência Da Computação", "Pedagogia", "Medicina", "Engenharia Civil", "Enfermagem", "Ciências Contábeis", "Educação Física", "Psicologia", "Recursos Humanos", "Engenhraia Elétrica")) %>%
+             c("Administração", "Direito", "Ciência Da Computação", "Pedagogia", "Medicina", "Engenharia Civil", "Enfermagem", "Ciências Contábeis", "Educação Física", "Psicologia", "Recursos Humanos", "Engenharia Elétrica")) %>%
     as.data.frame()
   
   bolsistas_curso_filter <- reactive({
@@ -174,7 +174,7 @@ server <- function(input, output) {
   })
   
   bolsistas_por_curso_sexo <- prouni %>%
-    filter(NOME_CURSO_BOLSA_NOVO %in% c("Administração", "Direito", "Ciência Da Computação", "Pedagogia", "Medicina", "Engenharia Civil", "Enfermagem", "Ciências Contábeis", "Educação Física", "Psicologia", "Recursos Humanos", "Engenhraia Elétrica")) %>%
+    filter(NOME_CURSO_BOLSA_NOVO %in% c("Administração", "Direito", "Ciência Da Computação", "Pedagogia", "Medicina", "Engenharia Civil", "Enfermagem", "Ciências Contábeis", "Educação Física", "Psicologia", "Recursos Humanos", "Engenharia Elétrica")) %>%
     group_by_(.dots=c("SEXO_BENEFICIARIO_BOLSA", "ANO_CONCESSAO_BOLSA", "NOME_CURSO_BOLSA_NOVO")) %>%
     summarize(total = n()) %>%
     as.data.frame()
